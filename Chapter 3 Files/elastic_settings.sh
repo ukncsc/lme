1c236df58a720ce5282bb62cada23c806c606f0b
Duncan <43746821+duncan-ncc@users.noreply.github.com>
2019-05-02 13:15:30 +0100

    Removing special characters and whitespace from elasticsearch script. Fixes #11

--- Chapter 3 Files/elastic_settings.sh
+++ Chapter 3 Files/elastic_settings.sh
@@ -1,5 +1,4 @@
 #set future index to always have no replicas
 curl -X PUT "localhost:9200/_template/number_of_replicas" -H 'Content-Type: application/json' -d' {  "template": "*",  "settings": {    "number_of_replicas": 0  }}'
-
 #set all current indices to have 0 replicas
 curl -X PUT "localhost:9200/_all/_settings" -H 'Content-Type: application/json' -d '{"index" : {"number_of_replicas" : 0}}'
