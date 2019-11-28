89c02008a8c5422309e2b91e8a7627dcc8c5de70
Duncan <43746821+duncan-ncc@users.noreply.github.com>
2019-11-28 13:03:51 +0000

    V0.2.0 Dashboard Release alongside dashboard automatic updates.

--- Chapter 3 Files/dashboard_update.sh
+++ Chapter 3 Files/dashboard_update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/ukncsc/lme/master/Chapter%204%20Files/status.json
-curl -X POST -k --cacert "/opt/lme/Chapter 3 Files/certs/root-ca.crt" --user elastic:dashboardupdatepassword -H "Content-Type: application/json" -H 'kbn-xsrf: true' --data @status.json "https://127.0.0.1/api/kibana/dashboards/import?exclude=index-pattern"
+wget https://raw.githubusercontent.com/ukncsc/lme/master/Chapter%204%20Files/dashboards%20v0.2.0.json -O status.json
+curl -X POST -k --cacert "/opt/lme/Chapter 3 Files/certs/root-ca.crt" --user elastic:dashboardupdatepassword -H "Content-Type: application/json" -H 'kbn-xsrf: true' --data @status.json "https://127.0.0.1/api/kibana/dashboards/import"
