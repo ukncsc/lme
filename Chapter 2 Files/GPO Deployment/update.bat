715e3fec66e97c1ab0e9e4fbc5e2999c7d2216e8
duncan-ncc <43746821+duncan-ncc@users.noreply.github.com>
2020-07-03 19:12:56 +0100

    changing install script to reflect the fact that network and hashing is now suitable in our recommended sysmon config

--- Chapter 2 Files/GPO Deployment/update.bat
+++ Chapter 2 Files/GPO Deployment/update.bat
@@ -53,7 +53,7 @@ xcopy %GLBSYSMONBIN% %SYSMONDIR% /y
 xcopy %GLBSYSMONCONFIG% %SYSMONDIR% /y
 xcopy %GLBSIGCHECK% %SYSMONDIR% /y
 chdir %SYSMONDIR%
-%SYSMONBIN% -i %SYSMONCONF% -accepteula -h md5,sha256 -n -l
+%SYSMONBIN% -i %SYSMONCONF% -accepteula
 sc config Sysmon64 start= auto
 goto :checkversion
 
